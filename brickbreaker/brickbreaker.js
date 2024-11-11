cae0162fcebe46c7029caa7c24935019f9e4afef
Kendallp613 <97863550+Kendallp613@users.noreply.github.com>
2024-11-10 20:32:24 -0500

    Update brickbreaker.js

--- brickbreaker/brickbreaker.js
+++ brickbreaker/brickbreaker.js
@@ -9,7 +9,7 @@ let paddleX = (canvas.width - paddleWidth) / 2;
 const paddleSpeed = 7;
 
 // Ball properties with adjustable speed factor
-const ballSpeedFactor = 0.9; // Adjust this factor to slow down the ball
+const ballSpeedFactor = 1.2; // Adjust this factor to slow down the ball
 const initialBallSpeedX = 4 * ballSpeedFactor;
 const initialBallSpeedY = -4 * ballSpeedFactor;
 const ball = {
